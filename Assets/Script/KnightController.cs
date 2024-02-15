d04621c7430dcfc2a88687cd279fc1682da52742
Zhamal <zhamal.baizen@gmail.com>
2024-02-16 01:08:27 +0600

    aniready

---
+++ Assets/Script/KnightController.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KnightController : MonoBehaviour
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private KeyCode _JumpKey;
+    [SerializeField] private float _moveSpeed = 5f;
+    [SerializeField] private Button _jumpButton;
+    [SerializeField] private Slider _moveSlider;
+    private bool isJump = false;
+
+    void Update()
+    {
+        _animator.SetFloat("Blend", _moveSlider.value);
+
+        Move(); 
+    }
+    private void OnEnable()
+    {
+        _jumpButton.onClick.AddListener(Jump);
+    }
+
+    private void OnDisable()
+    {
+        _jumpButton.onClick.RemoveAllListeners();
+    }
+
+    public void Move()
+    {
+        float inputDir = Input.GetAxis("Horizontal");
+
+        _animator.SetFloat("Blend", Mathf.Abs(inputDir));
+
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x + inputDir, transform.position.y, 0), Time.deltaTime * _moveSpeed);
+
+        if (Input.GetKeyDown(_JumpKey) && isJump != true) { _animator.SetTrigger("jump"); isJump = true; }
+
+
+    }
+
+    private void Jump()
+    {
+        _animator.SetTrigger("jump");
+    }
+}
+
+
